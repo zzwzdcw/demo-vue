64a1aa8b9aa5c30f279e4b047f60ac6f85dbb515
Jack-Zhang-1314 <zyj17715640603@gmail.com>
2022-12-03 17:58:27 +0800

    update: some problems

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,22 @@
+<!-- @format -->
+
 <template>
-  <div class="home" >
-    <button @click="aesvuetest()">
-      aes前端加密前端解密测试
-    </button>
+  <div class="home">
+    <button @click="aesvuetest()">aes前端加密前端解密测试</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  },
-  methods:{
-    aesvuetest(){
-      console.log("onLond");
+  methods: {
+    aesvuetest() {
+      console.log('onLond')
     },
     created() {
-      this.onLond();
+      this.onLond()
     },
   },
 }
